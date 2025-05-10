3ddcb26c49b3cebcdd73a08e9d6d8e350cc04b4c
asifmalikdev <asifhameed8944@gmail.com>
2025-05-10 08:58:09 +0500

    Generic api view completed learning

--- portfolio/portfolio/api4/urls.py
+++ portfolio/portfolio/api4/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from . import views
-from .views import StudentAPI
+from .views import (StudentAPI, StudentListMixin, StudentCreateMixin,
+                    StudentUpdateView, StudentRetrieveMixin, StudentDeleteMixin, StudentCRUDView)
 
 urlpatterns = [
 
     path('student_api/', views.student_api, name='student_api'),
     path('student_api/<int:pk>', views.student_api, name='student_api'),
     path('StudentAPI/',StudentAPI.as_view(), name="StudentAPI"),
-    path('StudentAPI/<int:pk>', StudentAPI.as_view(), name="StudentAPI")
+    path('StudentAPI/<int:pk>', StudentAPI.as_view(), name="StudentAPI"),
+    path('StudentListMixin/',StudentListMixin.as_view(), name = "StudentListMixin" ),
+    path('StudentListMixin/<int:pk>', StudentRetrieveMixin.as_view(), name="StudentRetrieveMixin"),
+    path('StudentCreateMixin/', StudentCreateMixin.as_view(), name="StudentCreateMixin"),
+    path('StudentUpdateView/<int:pk>', StudentUpdateView.as_view(), name="StudentUpdateView"),
+    path('StudentDeleteMixin/<int:pk>', StudentDeleteMixin.as_view(), name="StudentDeleteMixin" ),
+    path('StudentCRUDView/', StudentCRUDView.as_view(), name="StudentCRUDView"),
+    path('StudentCRUDView/<int:pk>', StudentCRUDView.as_view(), name="StudentCRUDView"),
 
 ]
--- portfolio/portfolio/api4/views.py
+++ portfolio/portfolio/api4/views.py
@@ -1,12 +1,129 @@
+from functools import partial
+
 from django.core.serializers import serialize
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .models import Student
 from .serializers import StudentSerializer
+from rest_framework.generics import GenericAPIView, UpdateAPIView
+from rest_framework.mixins import (ListModelMixin, RetrieveModelMixin,
+                                   CreateModelMixin, UpdateModelMixin, DestroyModelMixin)
+
+
+class StudentListMixin(GenericAPIView, ListModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class StudentCreateMixin(GenericAPIView, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def post(self,request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class StudentRetrieveMixin(GenericAPIView, RetrieveModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+class StudentUpdateView(UpdateModelMixin, GenericAPIView):
+
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+class StudentDeleteMixin(GenericAPIView, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+class StudentCRUDView(CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        if 'pk' in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class StudentAPI(APIView):
     def get(self, request, pk=None , format=None):
